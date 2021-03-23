56359632d1a5dabfe65af5f4856f8cbea6065402
Anne <38195995+annedranowski@users.noreply.github.com>
2021-03-22 20:43:11 -0400

    function now works. but not correct.

--- sage/mvy.py
+++ sage/mvy.py
@@ -4,9 +4,9 @@ from sage.symbolic.expression_conversions import polynomial
 
 # test inputs
 
-t1 = Tableau([[1,1,3],[2]])
-t2 = Tableau([[1,2]])
-n = 3
+t1 = Tableau([[1,3]])
+t2 = Tableau([[1,3],[2],[4]])
+n = 4
 
 # main 
 
@@ -48,7 +48,10 @@ svs = mu_vars(t1,t2)
 R = PolynomialRing(GF(101),svs) # no s!
 R.inject_variables()
 
-
+gen_str = ''
+for g in R.gens():
+    gen_str += str(g) + ', '
+S = PolynomialRing(GF(101), gen_str + 's')
 
 def insert_row(mat,row):
     return matrix(mat.rows()[:mat.nrows()]+[row]+mat.rows()[mat.nrows():])
@@ -72,12 +75,12 @@ def mu_matrix(t1,t2):
         row.append(companion_matrix(p.coefficients(x,sparse=False),format='bottom'))
         row += upper_row_matrix(i+1)
         symbMat.append(row)
-    return block_matrix(R['s'],symbMat)
+    return block_matrix(S,symbMat)
 
-# tmp = mu_matrix(t1,t2) 
-# T = tmp.change_ring(PolynomialRing(GF(7),tmp.variables()))  
+# tmp = mu_matrix(t1,t2)
+# T = tmp.change_ring(PolynomialRing(GF(7),tmp.variables()))
 
-# from sage.symbolic.expression_conversions import polynomial 
+# from sage.symbolic.expression_conversions import polynomial
 
 # Ts = T - polynomial(s,base_ring=GF(7))
 
@@ -86,7 +89,7 @@ def mu_matrices_over_gf(t1,t2):
     # T = tmp.change_ring(PolynomialRing(GF(7),tmp.variables()))
     return T,  T - polynomial(s,base_ring=GF(101))  
 
-# create submatrices 
+# create submatrices
 
 # size = mu[0]
 
@@ -131,7 +134,7 @@ def create_ideal(t1,t2):
     Tsubs,Tssubs = mu_submatrices_over_gf(mat,mats)
     rel = []
     # J_0 = ideal(0)
-    J_0 = R['s'].ideal([])
+    J_0 = S.ideal([]).primary_decomposition()
     size = mu[0]
     for i in range(1,m):
         size_0 = size
@@ -150,38 +153,37 @@ def create_ideal(t1,t2):
             rk = size - kerT
             rk_old_start = size_0 - kerT_old_start
             rel += minors_with_last_col(Tsub**j, mu[i], rk+1)
-            good_ideals = [] # not sure where best to init this 
-            print(rel)
-            print(ideal(rel).base_ring())
+            good_ideals = [] # not sure where best to init this
+            # print(ideal(rel).base_ring())
             # print(ideal(rel).groebner_basis())
-            if rel != []:
+            # if rel != []:
                 # J = ideal(ideal(rel).groebner_basis()).primary_decomposition()
                 # J = ideal(rel).primary_decomposition()
-                J = R['s'].ideal(rel)
-                for K in J:
-                    rk_old = rk_old_start
-                    t1_sub_old = t1_sub_old_start
-                    kerT_old = kerT_old_start
-                    res = True
-                    for k in range(1,len(t1_sub_old[0])+1):
-                        if [K.reduce(mnr) for mnr in (Tsub**k).minors(rk_old) if K.reduce(mnr) !=0] == []:
-                            res = False
-                            break 
-                        t1_sub_old = [l[1:] for l in t1_sub_old if l != []]
-                        kerT_old += len([l for l in t1_sub_old if l != []])
-                        rk_old = size_0 - kerT_old
-                    if res == True and K.gens() != [0] and True in (polynomial(s,base_ring=GF(7)).divides(ele) for ele in K.gens()):
+            J = S.ideal(rel).primary_decomposition()
+            for K in J:
+                rk_old = rk_old_start
+                t1_sub_old = t1_sub_old_start
+                kerT_old = kerT_old_start
+                res = True
+                for k in range(1,len(t1_sub_old[0])+1):
+                    if [K.reduce(mnr) for mnr in (Tsub**k).minors(rk_old) if K.reduce(mnr) !=0] == []:
                         res = False
-                    if res == True and J != J_0 and not True in [True in (str(v).startswith('x_{}_{}'.format(n,i+1)) for v in K.gens().variables()) for n in range(1,i+1)]:
-                        res = False 
-                    if res == True:
-                        good_ideals.append(K)
+                        break
+                    t1_sub_old = [l[1:] for l in t1_sub_old if l != []]
+                    kerT_old += len([l for l in t1_sub_old if l != []])
+                    rk_old = size_0 - kerT_old
+                if res == True and K.gens() != [0] and True in (polynomial(s,base_ring=GF(101)).divides(ele) for ele in K.gens()):
+                    res = False
+                if res == True and J != J_0 and not True in [True in (str(v).startswith('x_{}_{}'.format(n,i+1)) for v in K.gens().variables()) for n in range(1,i+1)]:
+                    res = False
+                if res == True:
+                    good_ideals.append(K)
             good = good_ideals[0]
             for K in good_ideals:
                 good = good.intersection(K)
             rel = good.gens()
             # J_0 = ideal(ideal(rel).groebner_basis()).primary_decomposition()
-            J_0 = ideal(rel).primary_decomposition()
+            J_0 = S.ideal(rel).primary_decomposition()
             t1_sub = [l[1:] for l in t1_sub if l != []]
             kerT += len([l for l in t1_sub if l != []])
         for j in range(1,len(t2_sub[0])+1):
@@ -191,44 +193,45 @@ def create_ideal(t1,t2):
             rk_old_start = size_0 - kerTs_old_start
             rel += minors_with_last_col((Tssub)**j, mu[i], rk+1)
             good_ideals = [] # not sure where best to init this either 
-            print(rel) # debug
-            if rel != []:
+            # print(rel) # debug
+            # if rel != []:
                 # J = ideal(ideal(rel).groebner_basis()).primary_decomposition()
-                J = ideal(rel).primary_decomposition()
-                for K in J:
-                    rk_old = rk_old_start
-                    t2_sub_old = t2_sub_old_start
-                    kerTs_old = kerTs_old_start
-                    res = True 
-                    for k in range(1,len(t2_sub_old[0])+1):
-                        if [K.reduce(mnr) for mnr in (Tssub_0**k).minors(rk_old) if K.reduce(mnr) != 0] == []:
-                            res = False
-                            break
-                        t2_sub_old = [l[1:] for l in t2_sub_old if l != []]
-                        kerTs_old += len([l for l in t2_sub_old if l != []])
-                        rk_old = size_0 - kerTs_old
-                    if res == True and K.gens() != [0] and True in (polynomial(s,base_ring=GF(7)).divides(ele) for ele in K.gens()):
+            J = S.ideal(rel).primary_decomposition()
+            for K in J:
+                rk_old = rk_old_start
+                t2_sub_old = t2_sub_old_start
+                kerTs_old = kerTs_old_start
+                res = True
+                for k in range(1,len(t2_sub_old[0])+1):
+                    if [K.reduce(mnr) for mnr in (Tssub_0**k).minors(rk_old) if K.reduce(mnr) != 0] == []:
                         res = False
-                    if res == True and J != J_0 and not True in [True in (str(v).startswith('x_{}_{}'.format(n,i+1)) for v in K.gens().variables()) for n in range(1,i+1)]:
-                        res = False
-                    if res == True:
-                        good_ideals.append(K)
-                good = good_ideals[0]
-                for K in good_ideals:
-                    good = good.intersection(K)
-                rel = good.gens()
-                if rel != []:
+                        break
+                    t2_sub_old = [l[1:] for l in t2_sub_old if l != []]
+                    kerTs_old += len([l for l in t2_sub_old if l != []])
+                    rk_old = size_0 - kerTs_old
+                if res == True and K.gens() != [0] and True in (polynomial(s,base_ring=GF(101)).divides(ele) for ele in K.gens()):
+                    res = False
+                if res == True and J != J_0 and not True in [True in (str(v).startswith('x_{}_{}'.format(n,i+1)) for v in K.gens().variables()) for n in range(1,i+1)]:
+                    res = False
+                if res == True:
+                    good_ideals.append(K)
+            good = good_ideals[0]
+            for K in good_ideals:
+                good = good.intersection(K)
+            rel = good.gens()
+                # if rel != []:
                     # J_0 = ideal(ideal(rel).groebner_basis()).primary_decomposition()
-                    J_0 = ideal(rel).primary_decomposition()
+            J_0 = S.ideal(rel).primary_decomposition()
             t2_sub = [l[1:] for l in t2_sub if l != []]
             kerTs += len([l for l in t2_sub if l != []])
     min_pol = T**(lambda_1[0])*(Ts)**(lambda_2[0])
-    print(min_pol) 
+    # print(min_pol)
     for e in min_pol:
-        print(e) # debug 
+    #     print(e) # debug
         rel += e
-    rel += polynomial(s,base_ring=GF(7)) 
-    print(rel) # debug
-    I = ideal(rel)
+    print(rel)
+    rel += [s]
+    # print(rel) # debug
+    I = S.ideal(rel)
     # return ideal(I.groebner_basis()).primary_decomposition()
     return I.primary_decomposition()
